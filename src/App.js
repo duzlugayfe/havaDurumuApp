98985e481a99217d42c2587fbd7fe795b85e16c7
Faruk Karaman <101044778+Faruk-Karaman@users.noreply.github.com>
2022-07-21 21:08:16 +0300

    last-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { usePosition } from "use-position";
+import HavaDurumu from "./components/HavaDurumu";
 
-function App() {
+const App = () => {
+  const [hDurumu, setHDurumu] = useState();
+  const { latitude, longitude } = usePosition();
+
+  const getHaDurumuData = async (lat, lon) => {
+    const key = process.env.WEATHERMAP_API_KEY;
+    const lang = navigator.language.split("-")[0];
+
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&lang=${lang}&units=metric`
+      );
+      setHDurumu(data);
+    } catch {
+      alert("Veri alinirken hata olustu.");
+    }
+  };
+
+  useEffect(() => {
+    latitude && longitude && getHaDurumuData(latitude, longitude);
+  }, [latitude, longitude]);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <h2>Hava Durumu</h2>
+      <HavaDurumu weather={hDurumu} />
     </div>
   );
-}
+};
 
 export default App;
